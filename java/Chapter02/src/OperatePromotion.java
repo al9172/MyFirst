3b7ff4f603148d93822bc9808455c45b222e98ce
LeeSeonmi_KOSMO <al917253@gmail.com>
2018-06-21 09:49:25 +0900

    자바 수업

---
+++ java/Chapter02/src/OperatePromotion.java
@@ -0,0 +1,12 @@
+
+public class OperatePromotion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        short num1 = 11;
+        short num2 = 22;
+        //short result = num1 + num2;
+        //System.out.println(result);
+	}
+
+}
---
+++ java/chapter.01/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+class FirstJavaProgram
+{
+   public static void main(String[] args)
+   {
+	  	 System.out.print("welcome to Java 1");
+		 System.out.print("First Java program 2");
+    }
+}
